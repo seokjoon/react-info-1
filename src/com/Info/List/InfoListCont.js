1460d0dba0af7b7459787e445c4bdc74632f4273
seokjoon <seokjoon@gmail.com>
2021-12-11 06:17:12 +0900

    redux

--- src/com/Info/List/InfoListCont.js
+++ src/com/Info/List/InfoListCont.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import InfoListCom from './InfoListCom'
 import { useDispatch, useSelector } from 'react-redux'
 import infoListReadRedux from '../../../redux/info/infoListReadRedux'
 
 const InfoListCont = () => {
+
+  const dispatch = useDispatch()
 
   const { error, items, pageLast, } = useSelector(({ infoListReadRedux, }) => ({
     error: infoListReadRedux.error,
@@ -11,24 +13,17 @@ const InfoListCont = () => {
     pageLast: infoListReadRedux.pageLast,
   }))
 
-  const dispatch = useDispatch()
-
-  useEffect(() => {
+  const readItems = useCallback(() => {
+    console.log('InfoListCont.readItems')
     dispatch(infoListReadRedux.read({ page: 1 }))
   }, [dispatch])
 
-  // const items = [ { content: 'foo', id: 1, title: 'foo', }, ]
-
-  const onTouchEnd = (e) => {
+  useEffect(() => {
     readItems()
-  }
-
-  const readItems = () => {
-    console.log('readItems complete')
-  }
+  }, [readItems])
 
   return (
-    <div onTouchEnd={onTouchEnd}>
+    <div onTouchEnd={readItems}>
 
       <button onClick={readItems}>pull to refresh</button>
 
