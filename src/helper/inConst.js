035a9c2adc585ce4eb6d0c58657aec65f5a7a5de
seokjoon <seokjoon@gmail.com>
2022-02-19 19:39:10 +0900

    rs

--- src/helper/inConst.js
+++ src/helper/inConst.js
@@ -1,8 +1,13 @@
 const inConst = {}
 
+
 inConst.path = {}
+
 inConst.path.index = process.env.REACT_APP_PATH
 inConst.path.info = `${inConst.path.index}/info`
 inConst.path.infoCat = `${inConst.path.index}/infoCat`
+
+inConst.path.user = `${inConst.path.index}/user`
+
 
 export default inConst
