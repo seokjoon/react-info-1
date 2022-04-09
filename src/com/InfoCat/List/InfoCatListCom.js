601ab5b609796ab4681d1ffe8be2c5009b70b063
seokjoon <seokjoon@gmail.com>
2022-04-09 19:17:29 +0900

    rs

--- src/com/InfoCat/List/InfoCatListCom.js
+++ src/com/InfoCat/List/InfoCatListCom.js
@@ -2,17 +2,20 @@ import React from 'react'
 
 const InfoCatListCom = ({ error, items, }) => {
 
+  const outErr = ( <div>{ JSON.stringify(error) }</div> )
+
   const outItems = items.map(item => (
-    <div>{ item.title && item.title }</div>
+    <div key={item._id}>{ item.title && item.title }</div>
   ))
 
   return (
     <div>
       InfoCatListCom
 
-      { (error) && <div>{ JSON.stringify(error) }</div> }
-
       { outItems }
+
+      { error && outErr }
+
     </div>
   )
 }
